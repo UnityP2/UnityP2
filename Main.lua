430de67bf0b0d8d9a69729f8f5fe446d6f4a0d08
Dective0101 <84044867+UnityP2@users.noreply.github.com>
2022-01-09 14:46:36 -0300

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,12 @@
+local discordinv = "https://discord.gg/xRkZMpAeWX"
+local d
+local f = pcall(function()
+    d = game:HttpGet("https://raw.githubusercontent.com/UnityP2/UnityP2/main/"..game.PlaceId..".lua")
+end)
+if f == true then
+    loadstring(d)()
+else
+  game.Players.LocalPlayer:Kick(SCRIPT ANTIGO FAVOR ENTRE NO NOSSO DISCORD PARA OBTER UM ATUALIZADO "..discordinv)
+  Invite()
+  setclipboard(discordinv)
+end
